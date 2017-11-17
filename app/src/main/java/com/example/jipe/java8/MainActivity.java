41684533f038c15950b5f686c0c67073d02aeeb2
Jean-Philippe Barbaud <jean-philippe.barbaud@schibsted.com>
2017-11-17 11:39:52 +0100

    commit

---
+++ app/src/main/java/com/example/jipe/java8/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.jipe.java8;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
